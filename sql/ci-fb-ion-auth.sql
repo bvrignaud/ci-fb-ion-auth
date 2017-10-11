03a766fde14d38b79b6a067cbc716da82249d3c6
Benoit VRIGNAUD <benoit.vrignaud@zaclys.net>
2017-10-11 17:42:40 +0200

    BUGFIX in database
    Missing avatar column in users table

--- sql/ci-fb-ion-auth.sql
+++ sql/ci-fb-ion-auth.sql
@@ -5,3 +5,6 @@ CREATE TABLE `facebook_user` (
   KEY `fk_facebook_user_users1_idx` (`users_id`),
   CONSTRAINT `fk_facebook_user_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `users` 
+ADD COLUMN `avatar` VARCHAR(45) NULL DEFAULT NULL;
